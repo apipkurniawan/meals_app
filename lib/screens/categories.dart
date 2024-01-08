eb7d24a4b6d07d3b21d43b9b71daeee3350150b8
apipkurniawan <davitaja92@gmail.com>
2024-01-08 11:13:18 +0700

    send data between screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/category.dart';
+import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _onSelectCategory(BuildContext context) {
-    print("yuhu");
+  void _onSelectCategory(BuildContext context, Category category) {
+    List<Meal> filteredMeal =
+        dummyMeals.where((el) => el.categories.contains(category.id)).toList();
+
     Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (ctx) => const MealsScreen(meals: [], title: ""),
+        builder: (ctx) =>
+            MealsScreen(meals: filteredMeal, title: category.title),
       ),
     );
   }
@@ -31,7 +36,7 @@ class CategoriesScreen extends StatelessWidget {
           for (final category in availableCategories)
             CategoryGridItem(
               category: category,
-              onTapCategory: () => _onSelectCategory(context),
+              onTapCategory: () => _onSelectCategory(context, category),
             )
         ],
       ),
