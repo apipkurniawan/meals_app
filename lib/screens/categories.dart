bd46b00daad4c373027d4801714f4934bc29fc61
apipkurniawan <davitaja92@gmail.com>
2024-01-09 10:49:01 +0700

    create tab bar

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -22,24 +22,21 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Pick your category")),
-      body: GridView(
-        padding: const EdgeInsets.all(16),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 20,
-            mainAxisSpacing: 20),
-        children: [
-          // availableCategories.map((category) => CategoryGridItem(category))
-          for (final category in availableCategories)
-            CategoryGridItem(
-              category: category,
-              onTapCategory: () => _onSelectCategory(context, category),
-            )
-        ],
-      ),
+    return GridView(
+      padding: const EdgeInsets.all(16),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20),
+      children: [
+        // availableCategories.map((category) => CategoryGridItem(category))
+        for (final category in availableCategories)
+          CategoryGridItem(
+            category: category,
+            onTapCategory: () => _onSelectCategory(context, category),
+          )
+      ],
     );
   }
 }
--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -4,9 +4,9 @@ import 'package:meals_app/screens/meal_details.dart';
 import 'package:meals_app/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key, required this.meals, required this.title});
+  const MealsScreen({super.key, required this.meals, this.title});
 
-  final String title;
+  final String? title;
   final List<Meal> meals;
 
   void _onSelectMeal(BuildContext context, Meal meal) {
@@ -54,8 +54,12 @@ class MealsScreen extends StatelessWidget {
       );
     }
 
+    if (title == null) {
+      return content;
+    }
+
     return Scaffold(
-      appBar: AppBar(title: Text(title)),
+      appBar: AppBar(title: Text(title!)),
       body: content,
     );
   }
